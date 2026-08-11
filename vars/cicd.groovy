2309b89041572c7ddbeb8e589b6ede34f497079a
Naveedqureshi929 <nq486402@gmail.com>
2026-08-11 15:05:26 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,9 +8,9 @@ def buildArtifact()
 }
 def deployTomcat(jobname,ip,context)
 {
-    sh"scp /var/lib/jenkins/workspace/Share/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+    sh"scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
-def runSelenium()
+def runSelenium(jobname)
 {
-    sh"java -jar /var/lib/jenkins/workspace/Share/testing.jar"
+    sh"java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
